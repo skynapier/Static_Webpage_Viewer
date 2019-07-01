ef7e08cb4b6bf709b52c132aacbb76646a71a5ee
tian <skynapier@outlook.com>
2019-07-01 18:51:04 +1200

    adapt mobile, temporary delete login input

--- js/check.js
+++ js/check.js
@@ -1,12 +1,28 @@
+
+
 function check() { 
     var userAgentInfo=navigator.userAgent; 
     var Agents =new Array("Android","iPhone","SymbianOS","Windows Phone","iPad","iPod"); 
-    var flag=true; 
+    var isMobile= false; 
     for(var v=0;v<Agents.length;v++) { 
-       if(userAgentInfo.indexOf(Agents[v])>0) { 
-         flag=false; 
-         break; 
-       } 
-     } 
-     return flag; 
+    if(userAgentInfo.indexOf(Agents[v])>0) { 
+        isMobile = true; 
+        break; 
+    } 
+    } 
+    return isMobile; 
+}
+let isMobile = check();
+
+if(isMobile){
+    var child = document.getElementsByClassName("wrap");
+    for(var i = 0; i <child.length; i++){
+        document.body.removeChild(child[i]);
+    }
+    
+    var child = document.getElementsByClassName("note");
+    for(var i = 0; i <child.length; i++){
+        document.body.removeChild(child[i]);
+    }
+
 }
